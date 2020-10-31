3a6d233260733bd82891bc9350148142f1f19a04
fengxiyao000 <751061411@qq.com>
2020-10-31 23:32:45 +0800

    init repo

---
+++ src/test/java/life/study/springboot/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package life.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
